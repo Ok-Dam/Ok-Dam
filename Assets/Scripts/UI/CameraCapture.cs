af777fd1e8b2fa3ecdb3c2a01d06a3f5906dff5c
김나영 <3725nadia@naver.com>
2025-05-23 23:42:16 +0900

    카메라 기능 완성
    실제이미지 나옴, 설명 추가

--- Assets/Scripts/UI/CameraCapture.cs
+++ Assets/Scripts/UI/CameraCapture.cs
@@ -7,6 +7,7 @@ public class CameraCapture : MonoBehaviour
 {
     public RawImage photoPreviewUI;  // 찍은 사진 보여줄 UI
     public GameObject photoPreviewPanel; // 패널 전체 (RawImage 포함)
+    public Texture2D predefinedPhoto; // 기와 이미지
 
     public AudioSource shutterSound; // 셔터 효과음
     public GameObject flashImage;    // 플래시 효과 이미지
@@ -30,9 +31,9 @@ public class CameraCapture : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         // 화면 캡처
-        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        tex.Apply();
+        //Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        //tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        //tex.Apply();
 
         // 촬영 효과(플래시)
         if (flashImage != null)
@@ -41,15 +42,19 @@ public class CameraCapture : MonoBehaviour
             yield return new WaitForSeconds(0.3f);
             flashImage.SetActive(false);
         }
-
+        //셔터소리
         if (shutterSound != null)
         {
             shutterSound.Play();
         }
 
         // 사진 보여주기
-        photoPreviewUI.texture = tex;
-        photoPreviewPanel.gameObject.SetActive(true);
+        //photoPreviewUI.texture = tex;
+        //photoPreviewPanel.gameObject.SetActive(true);
+
+        // 기와 사진 보여주기
+        photoPreviewUI.texture = predefinedPhoto;
+        photoPreviewPanel.SetActive(true);
 
 
     }
