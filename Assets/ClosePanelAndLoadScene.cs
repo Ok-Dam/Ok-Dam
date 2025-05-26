0ee4bc3145e5636dba6d410cc89c1e745715d5b5
CSY5316 <cococsy5316@naver.com>
2025-05-26 15:39:01 +0900

    OndolSimulScene 수정

---
+++ Assets/ClosePanelAndLoadScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClosePanelAndLoadScene : MonoBehaviour
+{
+    public GameObject panelToClose;     // ¥›¿ª ∆–≥Œ
+    public string sceneToLoadName;      // ¿Ãµø«“ æ¿ ¿Ã∏ß
+
+    public void CloseAndLoad()
+    {
+        if (panelToClose != null)
+            panelToClose.SetActive(false);
+
+        SceneManager.LoadScene(sceneToLoadName);
+    }
+}
