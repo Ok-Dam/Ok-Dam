ce62a58a251aa499f9950ff76c0b603b32a96b4a
이승언 <seungeon7878@gmail.com>
2025-09-24 11:30:24 +0900

    쉐이더 주석 + 이해

--- Assets/02. Scripts/Minigame/OndolPacMan/HeatMapManager.cs
+++ Assets/02. Scripts/Minigame/OndolPacMan/HeatMapManager.cs
@@ -27,8 +27,10 @@ public class HeatMapManager : MonoBehaviour
         gridWidth = gridManager.gridWidth;
         gridHeight = gridManager.gridHeight;
 
+        // 빨간색 된 그리드 기록
         heatedCells = new bool[gridWidth, gridHeight];
 
+        // 1. 투명 텍스쳐 생성 - 오버레이로 작동할 텍스쳐. 원본 텍스쳐랑 쭉 같이 떠있다. 투명이라 평소엔 원본만 있는 것처럼 보이는 거. 
         heatTexture = new Texture2D(gridWidth, gridHeight, TextureFormat.RGBA32, false);
         heatTexture.filterMode = FilterMode.Point;
 
@@ -40,7 +42,8 @@ public class HeatMapManager : MonoBehaviour
             return;
         }
 
-        // IMPORTANT: assign heatTexture to '_HeatTex' property of your custom shader!
+        // 2. 내 커스텀 쉐이더(OverlayHeat.shader가 지정돼있어야 함)한테 방금 만든 투명 텍스쳐 전달. 추후 heatTexture만 수정해도 알아서 전달됨.
+        // 쉐이더는 저 HeatTex랑 원본 텍스쳐를 투명도 따라 알아서 섞어서 잘 보여주는 역할
         floorRenderer.material.SetTexture("_HeatTex", heatTexture);
 
         // Optionally assign heat tint; you also have this in shader properties
@@ -62,12 +65,13 @@ public class HeatMapManager : MonoBehaviour
 
     public bool HeatCell(int x, int y)
     {
-
+        // 범위 밖이면
         if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight)
         {
             return false;
         }
 
+        // 이미 칠했으면
         if (heatedCells[x, y])
         {
             return false;
@@ -82,7 +86,10 @@ public class HeatMapManager : MonoBehaviour
 
         heatedCells[x, y] = true;
 
+        // 3. 플레이어가 이동 완료 후 pacPlayercontroller에서 좌표 주면 그걸로 아까의 투명 텍스쳐도 같은 위치를 빨갛게 칠한다.
         // 유니티 texture 2d 좌표랑 내가 쓰는 좌표 달라서 보정.
+        // gridWidth - 1 - x로 전달하면 x좌표가 21~1이고, -x-1로 하면 -2~-22다. 근데 둘 다 된다. 대체 왜지?????
+        Debug.Log($"Original x: {x}, Converted x: {-x-1}");
         heatTexture.SetPixel(-x-1, y, heatColor);
         heatTexture.Apply();
 
